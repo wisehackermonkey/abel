b35d3ffe2b5bbacff6288ea224db267be49bb271
Oran C <oranbusiness@gmail.com>
2021-12-12 22:07:49 -0700

    [added] localization for spanish

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import { Paper, Stack, Typography } from "@mui/material"
 import "@fontsource/special-elite"
 import "../css/Contact.css"
+import LocalizedStrings from 'react-localization';
+
+import localization from "../../localization.json"
+
+let strings = new LocalizedStrings(localization["contact"]);
+
 export default function Contact() {
     return (
         <Stack direction="row-reverse" >
             <Paper id="contact-box">
                 <Stack   id="contact-column">
-                    <Typography class="contact-font" >Contact</Typography>
+                    <Typography class="contact-font" >{strings.contact}</Typography>
                     <Typography class="contact-font" >+52 612-133-8634</Typography>
                     <Typography class="contact-font" >clavecharro2@gmail.com</Typography>
-                    <Typography class="contact-font" >Address: Austin loaches #43</Typography>
+                    <Typography class="contact-font" >{strings.address}: Austin loaches #43</Typography>
                 </Stack>
             </Paper>
         </Stack>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,16 +5,25 @@ import { Paper, Stack, Typography } from "@mui/material"
 
 import "@fontsource/special-elite"
 
+import LocalizedStrings from 'react-localization';
+
+import localization from "../../localization.json"
+
+let strings = new LocalizedStrings(localization["title"]);
+
 export default function Title() {
     return (
         <>
             <div id="title-row">
                 <Paper elevation={7} id="company-name" >
                     <Stack  id="title-stack">
-                        <Typography class="title-font" >Self</Typography>
-                        <Typography class="title-font"  fontFamily="elephant">Taught</Typography>
-                        <Typography class="title-font"   fontFamily="elephant">Jewelry</Typography>
-                    </Stack>
+                        {
+                            strings.title.split(" ").map((substring)=>(
+                                <Typography class="title-font" >{substring}</Typography>
+
+                            ))
+                        }
+                     </Stack>
                 </Paper>
             </div>
         </>
